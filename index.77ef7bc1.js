33aa1ed7b3438da2d6b18f5a254aee18285231ca
Seva Podolskiy <s_podolskiy@live.com>
2024-07-05 12:35:55 -0400

    make build

---
+++ index.77ef7bc1.js
@@ -0,0 +1,2 @@
+function e(e){var l=null==e?void 0:e.cells[(null==e?void 0:e.cells.length)-1],o=null==e?void 0:e.cells[1].cloneNode(!0);e.append(o,l)}e(document.querySelector("thead").rows[0]),document.querySelectorAll("tbody > tr").forEach(function(l){e(l)}),e(document.querySelector("tfoot").rows[0]);
+//# sourceMappingURL=index.77ef7bc1.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+function removerPaster(elem) {
+    const lastElem = elem?.cells[elem?.cells.length - 1];
+    const clone = elem?.cells[1].cloneNode(true);
+    elem.append(clone, lastElem);
+}
+removerPaster(document.querySelector("thead").rows[0]);
+document.querySelectorAll("tbody > tr").forEach((trEl)=>{
+    removerPaster(trEl);
+});
+removerPaster(document.querySelector("tfoot").rows[0]);
+
+//# sourceMappingURL=index.f75de5e1.js.map
